6a5ac1020381c0db592a91d47e433c19859d3403
ivanchang30901 <80505615+JustChangHTML5@users.noreply.github.com>
2023-07-21 22:26:27 -0600

    Revert "Added comments"
    This reverts commit 5c3924c7396a5cc987d317b71a85e39ca83a45f3.

--- Files.h
+++ Files.h
@@ -1,50 +1,36 @@
-#pragma once //Makes it so the compiler only includes the header file once
-#include <fstream> //Adds functions to read from files
-#include <string> //Adds a new datatype similar to and array of chars
-#include <vector> //Adds a new datatype similar to array
+#pragma once
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-//Define Files class. This manages reading files. I have not yet added features to write to files.
 class Files {
-
 public:
-
-	//fstream variables
 	ifstream instream;
 	ofstream outstream;
-
-	//current open file data
 	string curOpenFile;
 	string curFilePath; 
 
-	//constructor method
 	Files() {
 
 	}
 	
-	//defines the prototype method openFile which opens a file
-	//using a filepath to the instream variable defined early
 	void openFile(string filepath) {
 		curFilePath = filepath;
 		instream.open(curFilePath);
 		//outstream.open(curFilePath);
 	}
 
-	//closes any files in the instream variable
 	void closeFile() {
 		instream.close();
 		//outstream.close();
 	}
 
-	//define array prototypes that will have stuff written into them in the 
-	//cpp file.
 	void readFile(vector<string>& wordArr);
 	vector<string> returnLines(int lineCount);
 	
-	//deconstructor method
 	~Files() {
 
 	}
-
 };
